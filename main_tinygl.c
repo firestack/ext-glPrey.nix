353fbf5fcd5ac2c11dc001595fce0a367c29cf0e
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-05-13 22:47:46 -0500

    camera crap

--- main_tinygl.c
+++ main_tinygl.c
@@ -100,6 +100,76 @@ int len_palette;
  */
 
 /*
+ * gluLookAt
+ */
+
+void gluLookAt(GLfloat eyex, GLfloat eyey, GLfloat eyez, GLfloat centerx, GLfloat centery, GLfloat centerz, GLfloat upx, GLfloat upy, GLfloat upz)
+{
+	GLfloat m[16];
+	GLfloat x[3], y[3], z[3];
+	GLfloat mag;
+
+	/* Make rotation matrix */
+
+	/* Z vector */
+	z[0] = eyex - centerx;
+	z[1] = eyey - centery;
+	z[2] = eyez - centerz;
+	mag = sqrt( z[0]*z[0] + z[1]*z[1] + z[2]*z[2] );
+	if (mag) {  /* mpichler, 19950515 */
+		z[0] /= mag;
+		z[1] /= mag;
+		z[2] /= mag;
+	}
+
+	/* Y vector */
+	y[0] = upx;
+	y[1] = upy;
+	y[2] = upz;
+
+	/* X vector = Y cross Z */
+	x[0] =  y[1]*z[2] - y[2]*z[1];
+	x[1] = -y[0]*z[2] + y[2]*z[0];
+	x[2] =  y[0]*z[1] - y[1]*z[0];
+
+	/* Recompute Y = Z cross X */
+	y[0] =  z[1]*x[2] - z[2]*x[1];
+	y[1] = -z[0]*x[2] + z[2]*x[0];
+	y[2] =  z[0]*x[1] - z[1]*x[0];
+
+	/* mpichler, 19950515 */
+	/* cross product gives area of parallelogram, which is < 1.0 for
+		* non-perpendicular unit-length vectors; so normalize x, y here
+		*/
+
+	mag = sqrt( x[0]*x[0] + x[1]*x[1] + x[2]*x[2] );
+	if (mag) {
+		x[0] /= mag;
+		x[1] /= mag;
+		x[2] /= mag;
+	}
+
+	mag = sqrt( y[0]*y[0] + y[1]*y[1] + y[2]*y[2] );
+	if (mag) {
+		y[0] /= mag;
+		y[1] /= mag;
+		y[2] /= mag;
+	}
+
+	#define M(row,col)  m[col*4+row]
+	M(0,0) = x[0];  M(0,1) = x[1];  M(0,2) = x[2];  M(0,3) = 0.0;
+	M(1,0) = y[0];  M(1,1) = y[1];  M(1,2) = y[2];  M(1,3) = 0.0;
+	M(2,0) = z[0];  M(2,1) = z[1];  M(2,2) = z[2];  M(2,3) = 0.0;
+	M(3,0) = 0.0;   M(3,1) = 0.0;   M(3,2) = 0.0;   M(3,3) = 1.0;
+	#undef M
+
+	glMultMatrixf(m);
+
+	/* Translate Eye to Origin */
+	glTranslatef(-eyex, -eyey, -eyez);
+}
+
+/*
  * init
  */
 
@@ -184,14 +254,14 @@ int main(int argc, char **argv)
 
 		/* inputs */
 		if (platform_key(KEY_ESCAPE)) break;
-		if (platform_key(KEY_W)) gl_pos[0] += 8;
-		if (platform_key(KEY_A)) gl_pos[1] -= 8;
-		if (platform_key(KEY_S)) gl_pos[0] -= 8;
-		if (platform_key(KEY_D)) gl_pos[1] += 8;
-		if (platform_key(KEY_UP)) gl_rot[0] += 1;
-		if (platform_key(KEY_DOWN)) gl_rot[0] -= 1;
-		if (platform_key(KEY_LEFT)) gl_rot[1] += 1;
-		if (platform_key(KEY_RIGHT)) gl_rot[1] -= 1;
+		if (platform_key(KEY_W)) gl_pos[0] += 32;
+		if (platform_key(KEY_A)) gl_pos[1] -= 32;
+		if (platform_key(KEY_S)) gl_pos[0] -= 32;
+		if (platform_key(KEY_D)) gl_pos[1] += 32;
+		if (platform_key(KEY_UP)) gl_rot[0] += 8;
+		if (platform_key(KEY_DOWN)) gl_rot[0] -= 8;
+		if (platform_key(KEY_LEFT)) gl_rot[1] += 8;
+		if (platform_key(KEY_RIGHT)) gl_rot[1] -= 8;
 
 		/* gl */
 		glViewport(0, 0, (GLint)WIDTH, (GLint)HEIGHT);
@@ -200,10 +270,7 @@ int main(int argc, char **argv)
 		glFrustum(-1.0, 1.0, -h, h, 8, FLT_MAX);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		glTranslatef(gl_pos[0], gl_pos[1], gl_pos[2]);
-		glRotatef(gl_rot[0], 1, 0, 0);
-		glRotatef(gl_rot[1], 0, 1, 0);
-		glRotatef(gl_rot[2], 0, 0, 1);
+		gluLookAt(gl_pos[0], gl_pos[1], gl_pos[2], 0, 0, 0, 0, 1, 0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glPushMatrix();
@@ -218,6 +285,7 @@ int main(int argc, char **argv)
 	platform_quit();
 	bsp_free(bsp);
 	wad_free(wad);
+	glClose();
 
 	/* exit gracefully */
 	return 0;
